8d19b625b2e79cd92eeaca24f941644df92691a5
z01p4 <brulums@gmail.com>
2025-09-17 17:47:10 +0700

    <pesan_commit>

--- main/models.py
+++ main/models.py
@@ -1,7 +1,9 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 class News(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True) # tambahkan ini
     CATEGORY_CHOICES = [
         ('transfer', 'Transfer'),
         ('update', 'Update'),
